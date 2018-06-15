700f4941f96081eed71b4e146ab8bece6bc6b8a3
woliverhl <ohidalgoleal@gmail.com>
2018-06-14 22:01:46 -0400

    version inicial de eureka

---
+++ src/main/java/cl/usach/dminuteeureka/DMinuteEurekaApplication.java
@@ -0,0 +1,12 @@
+package cl.usach.dminuteeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DMinuteEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DMinuteEurekaApplication.class, args);
+	}
+}
